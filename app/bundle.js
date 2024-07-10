3e2a2cbebe96b0a52c58ef01990085364880b03a
KN <156647897+kn070648@users.noreply.github.com>
2024-07-10 17:22:15 +0800

    Completed the reveal on scroll behavior.

---
+++ app/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./app/assets/scripts/App.js":
+/*!***********************************!*\
+  !*** ./app/assets/scripts/App.js ***!
+  \***********************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Identifier 'RevealOnScroll' has already been declared (5:4)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n| import RevealOnScroll from \\\"./modules/RevealOnScroll\\\";\\n| \\n> let RevealOnScroll = new RevealOnScroll();\\n| let mobileMenu = new MobileMenu();\\n| \");\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./app/assets/scripts/App.js"]();
+/******/ 	
+/******/ })()
+;
